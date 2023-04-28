612549b979276b4037b42d3c65aea438b2d0d8b6
yudayuda78 <ariefyudha78@gmail.com>
2023-04-28 23:09:21 +0700

    menambah file sidebar dan content

--- index.php
+++ index.php
@@ -37,62 +37,12 @@
     <div class="container-lg">
         <div class="row">
             <!-- sidebar -->
-            <div class="col-lg-3">
-                <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-3 border mt-2">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                            data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
-                            aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
-                            aria-labelledby="offcanvasNavbarLabel" style="width:200px">
-                            <div class="offcanvas-header">
-                                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="offcanvas-body">
-                                <ul class="navbar-nav nav-pills flex-column justify-content-end flex-grow-1">
-                                    <li class="nav-item">
-                                        <a class="nav-link active link-light" aria-current="page" href="#"><i
-                                                class="bi bi-house-door"></i> Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link-dark" href="#"><i class="bi bi-cart4"></i> Order</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link-dark" href="#"><i class="bi bi-person"></i> Customer</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link-dark" href="#"><i class="bi bi-handbag"></i> Product</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link-dark" href="#"><i class="bi bi-envelope-paper"></i>
-                                            Report</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+            <?php include "sidebar.php" ?>
             <!-- end sidebar -->
 
 
             <!-- content -->
-            <div class="col-lg-9 mt-2">
-                <div class="card">
-                    <div class="card-header">
-                        Home
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ratione facere perspiciatis est minima nemo provident rerum debitis accusantium cupiditate repudiandae iste, laborum et sunt sed in suscipit praesentium a.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
+            <?php include "content.php" ?>
             <!-- end content -->
         </div>
 
---
+++ sidebar.php
@@ -0,0 +1,38 @@
+<div class="col-lg-3">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-3 border mt-2">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel" style="width:200px">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav nav-pills flex-column justify-content-end flex-grow-1">
+                        <li class="nav-item">
+                            <a class="nav-link active link-light" aria-current="page" href="#"><i
+                                    class="bi bi-house-door"></i> Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link-dark" href="#"><i class="bi bi-cart4"></i> Order</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link-dark" href="#"><i class="bi bi-person"></i> Customer</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link-dark" href="#"><i class="bi bi-handbag"></i> Product</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link-dark" href="#"><i class="bi bi-envelope-paper"></i>
+                                Report</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
