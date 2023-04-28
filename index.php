3dc4374d540e2c04bb5e6e2f9a571eb24d7880d0
yudayuda78 <ariefyudha78@gmail.com>
2023-04-28 23:12:54 +0700

    menambah file navbar

--- index.php
+++ index.php
@@ -13,26 +13,8 @@
 <body>
 
     <!-- navbar -->
-    <nav class="navbar navbar-expand bg-body-tertiary bg-primary sticky-top" data-bs-theme="dark">
-        <div class="container-lg">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Username
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end mt-2">
-                            <li><a class="dropdown-item" href="#">Profile</a></li>
-                            <li><a class="dropdown-item" href="#">Setting</a></li>
-                            <li><a class="dropdown-item" href="#">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include "navbar.php" ?>
+    <!-- akhir navbar -->
 
     <div class="container-lg">
         <div class="row">
---
+++ navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand bg-body-tertiary bg-primary sticky-top" data-bs-theme="dark">
+    <div class="container-lg">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Username
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end mt-2">
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="#">Setting</a></li>
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
