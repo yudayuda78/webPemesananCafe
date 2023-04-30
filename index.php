4acace0536ee5d845a6707fc2b3407a77fc49f76
yudayuda78 <ariefyudha78@gmail.com>
2023-04-30 23:06:47 +0700

    mengedit index dan navbar

--- index.php
+++ index.php
@@ -13,7 +13,26 @@
 <body>
 
     <!-- navbar -->
-    <?php include "navbar.php" ?>
+    <nav class="navbar navbar-expand bg-body-tertiary bg-primary sticky-top" data-bs-theme="dark">
+        <div class="container-lg">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Username
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end mt-2">
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li><a class="dropdown-item" href="#">Setting</a></li>
+                            <li><a class="dropdown-item" href="#">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <!-- akhir navbar -->
 
     <div class="container-lg">
