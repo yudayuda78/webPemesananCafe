3096c81f975ebc0b6fe39d3b1e9a361175189688
yudayuda78 <ariefyudha78@gmail.com>
2023-05-08 23:57:29 +0700

    mengubah side bar menjadi dinamis

--- sidebar.php
+++ sidebar.php
@@ -14,20 +14,20 @@
                 <div class="offcanvas-body">
                     <ul class="navbar-nav nav-pills flex-column justify-content-end flex-grow-1">
                         <li class="nav-item">
-                            <a class="nav-link active link-light" aria-current="page" href="#"><i
+                            <a class="nav-link ps-2 <?php echo (isset($_GET['x']) && $_GET['x']== 'home') ? 'active link-light' : 'link-dark' ; ?> " aria-current="page" href="index.php?x=home"><i
                                     class="bi bi-house-door"></i> Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link link-dark" href="order.php"><i class="bi bi-cart4"></i> Order</a>
+                            <a class="nav-link <?php echo (isset($_GET['x']) && $_GET['x']== 'order') ? 'active link-light' : 'link-dark' ; ?>" href="order.php?x=order"><i class="bi bi-cart4"></i> Order</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link link-dark" href="customer.php"><i class="bi bi-person"></i> Customer</a>
+                            <a class="nav-link <?php echo (isset($_GET['x']) && $_GET['x']== 'customer') ? 'active link-light' : 'link-dark' ; ?>" href="customer.php?x=customer"><i class="bi bi-person"></i> Customer</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link link-dark" href="product.php"><i class="bi bi-handbag"></i> Product</a>
+                            <a class="nav-link <?php echo (isset($_GET['x']) && $_GET['x']== 'product') ? 'active link-light' : 'link-dark' ; ?>" href="product.php?x=product"><i class="bi bi-handbag"></i> Product</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link link-dark" href="report.php"><i class="bi bi-envelope-paper"></i>
+                            <a class="nav-link <?php echo (isset($_GET['x']) && $_GET['x']== 'report') ? 'active link-light' : 'link-dark' ; ?>" href="report.php?x=report"><i class="bi bi-envelope-paper"></i>
                                 Report</a>
                         </li>
                     </ul>
