75ae0aac97ac4bb44a1a7508f8c6d9b000c8d085
yudayuda78 <ariefyudha78@gmail.com>
2023-05-09 00:05:02 +0700

    menambahkan fitur pada sidebar

---
+++ home.php
@@ -0,0 +1,18 @@
+<div class="col-lg-9 mt-2">
+    <div class="card">
+        <div class="card-header">
+            Home
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Ini adalah halaman beranda</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                Lorem ipsum
+                dolor sit amet, consectetur adipisicing elit. Accusamus ratione facere perspiciatis est
+                minima nemo
+                provident rerum debitis accusantium cupiditate repudiandae iste, laborum et sunt sed in
+                suscipit
+                praesentium a.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
