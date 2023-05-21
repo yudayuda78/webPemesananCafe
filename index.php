e1bec115bb6df0f8aa33fa34325fcf0bf936afff
yudayuda78 <ariefyudha78@gmail.com>
2023-05-22 00:30:04 +0700

    mengedit fitur main

--- index.php
+++ index.php
@@ -1,80 +1,18 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aplikasi Pemesanan</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-</head>
-
-<body>
-
-    <!-- navbar -->
-    <nav class="navbar navbar-expand bg-body-tertiary bg-primary sticky-top" data-bs-theme="dark">
-        <div class="container-lg">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Username
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end mt-2">
-                            <li><a class="dropdown-item" href="#">Profile</a></li>
-                            <li><a class="dropdown-item" href="#">Setting</a></li>
-                            <li><a class="dropdown-item" href="#">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- akhir navbar -->
-
-    <div class="container-lg">
-        <div class="row">
-            <!-- sidebar -->
-            <?php include "sidebar.php" ?>
-            <!-- end sidebar -->
-
 
             <!-- content -->
             <?php if (isset($_GET['x']) && $_GET['x']== 'home'){
-                include "home.php";
+                include "main.php";
                 }elseif(isset($_GET['x']) && $_GET['x']=='order'){
-                    include "order.php";
+                    include "main.php";
                 }elseif(isset($_GET['x']) && $_GET['x']=='product'){
-                    include "product.php";
+                    include "main.php";
                 }elseif(isset($_GET['x']) && $_GET['x']=='customer'){
-                    include "customer.php";
+                    include "main.php";
                 }elseif(isset($_GET['x']) && $_GET['x']=='report'){
-                    include "report.php";
+                    include "main.php";
+                }elseif(isset($_GET['x']) && $_GET['x']=='login'){
+                    include "login.php";
                 }else{
-                    include "home.php";
+                    include "main.php";
                 }; ?>
             <!-- end content -->
-        </div>
-
-
-        <div class="fixed-bottom text-center mb-2">
-            Copyright 2023 Yudayuda
-        </div>
-    </div>
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
