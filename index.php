26697af3ac6663e3b43566d056cc5bbd8832d8ac
yudayuda78 <ariefyudha78@gmail.com>
2023-05-23 23:56:04 +0700

    mengedit index

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
             <!-- content -->
             <?php if (isset($_GET['x']) && $_GET['x']== 'home'){
                 include "main.php";
+                $page = "home.php";
+                include "main.php";
                 }elseif(isset($_GET['x']) && $_GET['x']=='order'){
                     include "main.php";
                 }elseif(isset($_GET['x']) && $_GET['x']=='product'){
